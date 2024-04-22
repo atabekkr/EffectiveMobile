6cd01f04d76ea46642fc452367e228a4b6ea52ec
atabekkr <atabekdev@gmail.com>
2024-04-22 23:56:12 +0500

    added plug screen for all bottom nav view

--- app/src/main/java/com/karsoft/effectivemobile/presenter/ui/hotels/HotelsFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.karsoft.effectivemobile.presenter.ui.hotels
-
-import androidx.fragment.app.Fragment
-import com.karsoft.effectivemobile.R
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class HotelsFragment : Fragment(R.layout.fragment_hotels) {
-}
--- app/src/main/java/com/karsoft/effectivemobile/presenter/ui/subscribes/SubscribesFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.karsoft.effectivemobile.presenter.ui.subscribes
-
-import androidx.fragment.app.Fragment
-import com.karsoft.effectivemobile.R
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class SubscribesFragment: Fragment(R.layout.fragment_subscribes) {
-}
